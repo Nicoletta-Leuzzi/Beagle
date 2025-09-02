1dfec80c712e59930c54e31e108000f7263d9eb7
Marco Mandelli <119368321+astro-marco@users.noreply.github.com>
2025-09-02 19:54:11 +0200

    Aggiunte entità Room per i models e aggiunta RecyclerView a History
    Introdotte annotazioni e relazioni Room a Conversation, Message, Pet, e User per integrare il database.
    Creato ConversationAdapter per mostrare le conversazioni in una RecyclerView
    Aggiornato il layout dell'item_conversation.
    Aggiornato navigation graph per supportare il passaggio di conversationId come argument.

--- app/src/main/java/com/example/beagle/model/Pet.java
+++ app/src/main/java/com/example/beagle/model/Pet.java
@@ -1,5 +1,20 @@
 package com.example.beagle.model;
 
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
+
+@Entity(
+        foreignKeys = @ForeignKey(
+                entity = User.class,
+                parentColumns = "idToken",
+                childColumns = "idToken",
+                onDelete = ForeignKey.CASCADE,
+                onUpdate = ForeignKey.CASCADE
+        ),
+        indices = { @Index(value = {"idToken", "name"}, unique = true),
+                @Index("idToken") }
+)
 public class Pet {
     private String name;
 
