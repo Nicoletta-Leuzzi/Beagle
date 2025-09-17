eeca1fbfcb656cc38c7ea406803f3036b474ba09
Marco Mandelli <119368321+astro-marco@users.noreply.github.com>
2025-09-17 20:12:43 +0200

    Riordinati metodi in IMessageResponseCallback

--- app/src/main/java/com/example/beagle/repository/message/IMessageResponseCallback.java
+++ app/src/main/java/com/example/beagle/repository/message/IMessageResponseCallback.java
@@ -7,23 +7,19 @@ import java.util.List;
 
 public interface IMessageResponseCallback {
 
+    void onSuccessFromAPI(ChatCompletionResponse response, long conversationId, int seq);
     void onSuccessReadFromRemote(List<Message> messageList, long conversationId);
     void onSuccessReadFromLocal(List<Message> messageList);
+
     void onSuccessWriteFromRemote(Message message);
+    void onSuccessWriteAIFromLocal(List<Message> allMessages, Message message);
     void onSuccessWriteFromLocal(List<Message> allMessages, Message message, long conversationId, int seq);
     void onSuccessUpdateFromLocal(List<Message> messageList);
 
-    void onFailureFromRemote(Exception exception);
     void onFailureReadFromRemote(Exception exception);
     void onFailureReadFromLocal(Exception exception);
     void onFailureWriteFromRemote(Exception exception);
     void onFailureWriteFromLocal(Exception exception);
     void onFailureUpdateFromLocal(Exception exception);
-
-    // DA CAMBIARE IL PARAMETRO CON LA RISPOSTA DELL'AI
-    void onSuccessFromAPI(ChatCompletionResponse response, long conversationId, int seq);
-
-    void onSuccessWriteAIFromLocal(List<Message> allMessages, Message message);
-
 
 }
